8a4937a97a0d681b3fe3f82971b2da9d1022a3ef
Minglei Ma <mingleim@ece.ubc.ca>
2017-12-02 19:32:19 -0800

    Update customized.py

--- customized.py
+++ customized.py
@@ -5,10 +5,11 @@ class PhysicalModel(MingLeiModel):
     def __init__(self, init_hidden_vars, init_params):
         super(PhysicalModel, self).__init__(init_hidden_vars, init_params)
 
-    def observe(self, params):
+    def observe(self, slot, chn):
         pass
         # TODO: return p3 from your machine (in power unit)
-
+        return hp816x_instr_.hp816x().readPWM(slot, chn)
+    
     def set_params(self, params):
         self.params = params
         # TODO: set theta1 and theta2 to your machine. params=[theta1, theta2]
