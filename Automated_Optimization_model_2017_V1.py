f9ddb098584eb0448311d8c9b2ea3acfada7ea92
Minglei Ma <mingleim@ece.ubc.ca>
2017-12-08 18:01:25 -0800

    Update Automated_Optimization_model_2017_V1.py

--- Automated_Optimization_model_2017_V1.py
+++ Automated_Optimization_model_2017_V1.py
@@ -13,9 +13,12 @@ reload (HP11896A)
 import random
 from random import randint
 import customized
+reload(customized)
 from customized import PhysicalModel
 import Ke26XXA
+from Ke26XXA import Ke26XXA
 import hp816x_instr
+import HP11896A
 
 #%%
 k1 = Ke26XXA() #Keithley instrument
@@ -26,9 +29,19 @@ HPMainFrame.setPWMAveragingTime(1, 1, 10e-3)   #unit: s
 HPMainFrame.setPWMAveragingTime(1, 0, 10e-3)
 Init_h12 = [0.5, 0.5]
 Init_theta12 = [0.5, 0.5]
-Imax = 16e-3
+Imax = 36e-3
 #%%
-model = customized.PhysicalModel(Init_h12, Init_theta12, 1, 0, 'a', 'b', k1, Imax);
+#connect the polarization controller 11896A
+Input1 = HP11896A.HP11896A()
+Input1.connect('GPIB0::21::INSTR')
+Input1.SetPaddlePos('1', (random.randint(0,999)))
+Input1.SetPaddlePos('2', (random.randint(0,999)))  
+Input1.SetPaddlePos('3', (random.randint(0,999)))
+Input1.SetPaddlePos('4', (random.randint(0,999)))
+#%%
+model = customized.PhysicalModel(Init_h12, Init_theta12, 1, 1, k1, 'b', 'a', Imax);
+model.on()
 model.train_and_optimize()
+model.off()
                             
 
