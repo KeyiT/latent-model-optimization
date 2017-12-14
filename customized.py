f5dd583af6095183deca33ef1a80cdcb7cc7ae29
Minglei Ma <mingleim@ece.ubc.ca>
2017-12-13 17:32:36 -0800

    Update customized.py

--- customized.py
+++ customized.py
@@ -5,7 +5,9 @@ import Ke26XXA
 reload(Ke26XXA)
 from Ke26XXA import Ke26XXA        
 import math
-
+import numpy as np
+#from sympy.slovers import solve
+#from sympy import Symbol
 
 class PhysicalModel(MingLeiModel):
     def __init__(self, init_hidden_vars, init_params, opt_slot, opt_chn, keith_dev, keith_chn1, keith_chn2, keith_imax):
@@ -13,32 +15,65 @@ class PhysicalModel(MingLeiModel):
         self.opt_chn = opt_chn
         self.keith_dev = keith_dev
         self.keith_chn1 = keith_chn1
-        self.keith_chn2 = keith_chn2
-        self.keith_imax = keith_imax
+        self.keith_chn2 = keith_chn2       
+        self.keith_imax = keith_imax 
+        self.hp_mainframe = hp816x_instr.hp816x() 
         super(PhysicalModel, self).__init__(init_hidden_vars, init_params)
+        print('ok1')
 
     def observe(self, params):
         # TODO: return p3 from your machine (in power unit)
         self.set_params(params)
-        time.sleep(0)
-        return hp816x_instr.hp816x().readPWM(self.opt_slot, self.opt_chn)
+        time.sleep(0.001)  ##unit: s
+        p3 = self.hp_mainframe.readPWM(self.opt_slot, self.opt_chn)
+        p3_dBm = 10*np.log10(p3)+30
+        print(p3_dBm)
+        return p3
+
+    def hpmainframe_connect(self, visaAddr):
+        self.hp_mainframe.connect(visaAddr, reset=0, forceTrans=1) #connects to the laser
+  
+    def setAvgtime(self, Avgtime):
+        self.hp_mainframe.setPWMAveragingTime(1, 1, Avgtime)   #unit: s
+        self.hp_mainframe.setPWMAveragingTime(1, 0, Avgtime)
+        
+    def setPWMUnit(self, Unit):
+        self.hp_mainframe.setPWMPowerUnit(self.opt_slot, 0, Unit)
+        self.hp_mainframe.setPWMPowerUnit(self.opt_slot, 1, Unit)
  
     def on(self):
-        self.keith_dev.outputEnable(self.keith_ch1, True)
-        self.keith_dev.outputEnable(self.keith_ch2, True)
+        self.keith_dev.outputEnable(self.keith_chn1, True)
+        self.keith_dev.outputEnable(self.keith_chn2, True)
         
     def off(self):
-        self.keith_dev.outputEnable(self.keith_ch1, False)
-        self.keith_dev.outputEnable(self.keith_ch2, False)    
+        self.keith_dev.outputEnable(self.keith_chn1, False)
+        self.keith_dev.outputEnable(self.keith_chn2, False)    
     
     def set_params(self, params):
         self.params = params
-        alpha = 100.8
-        R = 300
-        beta = 2.962
+        alpha = 97.08
+        beta = 0.02539
+        R = 200
+        print(params)
+#        a = []
+#        b = []
+#        eqn1 = a[0]*current1**4 + a[1]*current1**3 + a[2]*current1**2 - (params[0] - beta)/alpha
+#        eqn2 = b[0]*current2**4 + b[1]*current1**3 + b[2]*current1**2 - (params[1] - beta)/alpha
+#        slove(eqn1, current1, )
+        
         # TODO: set theta1 and theta2 to your machine. params=[theta1, theta2]
-        current1 = math.sqrt((params[0])/(alpha*R))
-        current2 = math.sqrt((params[1])/(alpha*R))        
+        if params[0] < beta and params[1] < beta:
+            current1 = math.sqrt((params[0]-beta+2*math.pi)/(alpha*R))
+            current2 = math.sqrt((params[1]-beta+2*math.pi)/(alpha*R))
+        if params[0] < beta and params[1] > beta:
+            current1 = math.sqrt((params[0]-beta+2*math.pi)/(alpha*R))
+            current2 = math.sqrt((params[1]-beta)/(alpha*R))
+        if params[0] > beta and params[1] < beta:
+            current1 = math.sqrt((params[0]-beta)/(alpha*R))
+            current2 = math.sqrt((params[1]-beta+2*math.pi)/(alpha*R))
+        if params[0] > beta and params[1] > beta:
+            current1 = math.sqrt((params[0]-beta)/(alpha*R))
+            current2 = math.sqrt((params[1]-beta)/(alpha*R))         
         if current1 > self.keith_imax + 1e-3:
             print('current larger than maximum channel1 current')
             current1 = math.sqrt((params[0]-beta-2*math.pi)/(alpha*R))
